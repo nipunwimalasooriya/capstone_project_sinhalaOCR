d41576b4acbdb3f225699bc57fcc1d4fda5f92bd
Nipun Wimalasooriya <40894068+nipunwimalasooriya@users.noreply.github.com>
2024-04-15 02:26:09 +0530

    Update ocr.py

--- backEnd/ocr.py
+++ backEnd/ocr.py
@@ -5,7 +5,7 @@ import pytesseract
 # Assuming Tesseract is installed and in the system PATH on Ubuntu
 pytesseract.pytesseract.tesseract_cmd = 'tesseract'  # No need for full path
 
-async def read_image(img_path, lang='sin', save_processed_image=True, processed_image_path="/tmp/processed_image.jpg"):
+async def read_image(img_path, lang='sin'):
     try:
         # Read image using OpenCV
         img = cv2.imread(img_path)
@@ -20,41 +20,9 @@ async def read_image(img_path, lang='sin', save_processed_image=True, processed_
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
         opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
 
-        if save_processed_image:
-            cv2.imwrite(processed_image_path, opening)
-
         # Apply OCR using Pytesseract to detect Sinhala Unicode language characters
-        return pytesseract.image_to_string(opening, lang=lang)
-    except:
-        return "[ERROR] Unable to process file: {0}".format(img_path)
+        text = pytesseract.image_to_string(opening, lang=lang)
+        return text
+    except Exception as e:
+        return f"[ERROR] Unable to process file: {img_path}, Error: {e}"
 
-
-async def read_images_from_dir(dir_path, lang='sin', write_to_file=False):
-    """
-    Performs OCR on all images present in a directory
-
-    :dir_path: str, path to the directory of images
-    :lang: str, language to be used while conversion (optional, default is english)
-
-    Returns
-    :converted_text: dict, mapping of filename to converted text for each image
-    """
-
-    converted_text = {}
-    for file_ in os.listdir(dir_path):
-        if file_.endswith(('png', 'jpeg', 'jpg')):
-            text = await read_image(os.path.join(dir_path, file_), lang=lang)
-            converted_text[os.path.join(dir_path, file_)] = text
-    if write_to_file:
-        for file_path, text in converted_text.items():
-            _write_to_file(text, os.path.splitext(file_path)[0] + ".txt")
-    return converted_text
-
-
-def _write_to_file(text, file_path):
-    """
-    Helper method to write text to a file
-    """
-    print("[INFO] Writing text to file: {0}".format(file_path))
-    with open(file_path, 'w') as fp:
-        fp.write(text)
